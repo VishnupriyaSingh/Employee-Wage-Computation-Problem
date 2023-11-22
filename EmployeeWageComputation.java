ede13581c0a3d70f43f51086b568b2d24eee5258
Vishnupriya <priya.singh9349@gmail.com>
2023-11-22 07:11:20 +0530

    Code for employee attendance added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,15 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        if (isEmployeePresent()) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+
+    public static boolean isEmployeePresent() {
+        return Math.random() > 0.5;
     }
 }
