4bd0e6174db45fd5f74b25d041ef7d7751476c0d
Vishnupriya <priya.singh9349@gmail.com>
2023-11-22 07:38:33 +0530

    Switch Case Functionality added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,25 +4,30 @@ public class EmployeeWageComputation {
     private static final int FULL_DAY_HOUR = 8;
     private static final int PART_TIME_HOUR = 4;
 
+    private static final int ABSENT = 0;
+    private static final int PART_TIME = 1;
+    private static final int FULL_TIME = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int employeeType = getEmployeeType();
 
         switch (employeeType) {
-            case 0:
+            case ABSENT:
                 System.out.println("Employee is Absent");
                 System.out.println("Daily Employee Wage: 0");
                 break;
-            case 1:
+            case PART_TIME:
                 System.out.println("Employee is Part-time");
-                int partTimeWage = calculateDailyWage(PART_TIME_HOUR);
-                System.out.println("Daily Employee Wage: " + partTimeWage);
+                System.out.println("Daily Employee Wage: " + calculateDailyWage(PART_TIME_HOUR));
                 break;
-            case 2:
+            case FULL_TIME:
                 System.out.println("Employee is Full-time");
-                int fullTimeWage = calculateDailyWage(FULL_DAY_HOUR);
-                System.out.println("Daily Employee Wage: " + fullTimeWage);
+                System.out.println("Daily Employee Wage: " + calculateDailyWage(FULL_DAY_HOUR));
+                break;
+            default:
+                System.out.println("Invalid Employee Type");
                 break;
         }
     }
@@ -30,11 +35,11 @@ public class EmployeeWageComputation {
     public static int getEmployeeType() {
         double randomNumber = Math.random();
         if (randomNumber < 0.33)
-            return 0;
+            return ABSENT;
         else if (randomNumber < 0.66)
-            return 1;
+            return PART_TIME;
         else
-            return 2;
+            return FULL_TIME;
     }
 
     public static int calculateDailyWage(int hours) {
