0f3b1715e733537118b82baf98b1a73538f7eb5e
Vishnupriya <priya.singh9349@gmail.com>
2023-11-23 22:47:45 +0530

    Added the Refactored Code

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -11,7 +11,10 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        computeEmployeeWage();
+    }
 
+    private static void computeEmployeeWage() {
         int totalWage = 0;
         int totalWorkingHours = 0;
         int workingDays = 0;
@@ -22,17 +25,14 @@ public class EmployeeWageComputation {
             switch (employeeType) {
                 case ABSENT:
                     System.out.println("Day " + (day + 1) + ": Employee is Absent");
-                    System.out.println("Hours worked: 0");
                     break;
                 case PART_TIME:
                     System.out.println("Day " + (day + 1) + ": Employee is Part-time");
-                    System.out.println("Hours worked: " + PART_TIME_HOUR);
                     totalWage += calculateDailyWage(PART_TIME_HOUR);
                     totalWorkingHours += PART_TIME_HOUR;
                     break;
                 case FULL_TIME:
                     System.out.println("Day " + (day + 1) + ": Employee is Full-time");
-                    System.out.println("Hours worked: " + FULL_DAY_HOUR);
                     totalWage += calculateDailyWage(FULL_DAY_HOUR);
                     totalWorkingHours += FULL_DAY_HOUR;
                     break;
@@ -54,14 +54,14 @@ public class EmployeeWageComputation {
         System.out.println("Total Working Days: " + workingDays);
     }
 
-    public static int getEmployeeType() {
+    private static int getEmployeeType() {
         double randomNumber = Math.random();
         if (randomNumber < 0.33) return ABSENT;
         else if (randomNumber < 0.66) return PART_TIME;
         else return FULL_TIME;
     }
 
-    public static int calculateDailyWage(int hours) {
+    private static int calculateDailyWage(int hours) {
         return WAGE_PER_HOUR * hours;
     }
 }
